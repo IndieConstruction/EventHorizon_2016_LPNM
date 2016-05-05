137645fb138c4c07e40f9040074ebdbabd1a4ee0
Amir Basyouni <amir.basyouni@gmail.com>
2016-05-05 13:02:34 +0200

    Aggiunta CoinBonus

---
+++ UnityProject/Assets/Scripts/Bonus.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+namespace EH.LPNM
+{
+    public class Bonus : MonoBehaviour
+    {
+        GameController gc;
+        public int BonusPoints;
+        public int BonusMultiplier;
+        // Use this for initialization
+        void Awake()
+        {
+            gc = FindObjectOfType<GameController>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        }
+        void OnTriggerEnter(Collider other) {
+            gc.scoreCounter = +BonusPoints;
+            gc.scoreCounter = +BonusMultiplier;
+        }
+    }
+}
