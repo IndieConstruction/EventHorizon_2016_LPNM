503c35c141d9ea6c8ddb9d177d90f29b1b7b7be9
Amir Basyouni <amir.basyouni@gmail.com>
2016-05-19 13:51:15 +0200

    Eureka!

--- UnityProject/Assets/Scripts/CameraRotation.cs
+++ UnityProject/Assets/Scripts/CameraRotation.cs
@@ -3,13 +3,31 @@ using System.Collections;
 
 public class CameraRotation : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    public Transform target;
+    public bool isRotating;
+
+    void Update() {
+        //transform.LookAt(target.position);
+
+        var targetRotation = Quaternion.LookRotation(target.transform.position - transform.position, Vector3.forward);
+
+        // Smoothly rotate towards the target point.
+        transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, 1 * Time.deltaTime);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "PlayerBorder")
+        {
+           // float step = 10;
+           // transform.rotation = Quaternion.RotateTowards(transform.rotation, target.rotation, step);
+            
+            //transform.rotation = new Quaternion(transform.rotation.x, 0, transform.rotation.z, transform.rotation.w);
+
+        }
+    }
 }
+    
+
+       
+       
--- UnityProject/Assets/Scripts/Player.cs
+++ UnityProject/Assets/Scripts/Player.cs
@@ -6,8 +6,9 @@ public class Player : MonoBehaviour {
 	public string Letter;
 	public MeshFilter[] MeshesLetter;
     public int PlayerLife;
+    //public Transform target;
 	void Start(){
-	//Letter = "A";
+	Letter = "A";
 	}
 	void Update(){
 
@@ -17,46 +18,56 @@ public class Player : MonoBehaviour {
 		}
 
 
-	public void MeshChange (string rightLetter){
+        public void MeshChange(string rightLetter)
+        {
+
+            MeshFilter rightMeshLetter;
+            rightMeshLetter = GetComponentInChildren<MeshFilter>();
+            switch (rightLetter)
+            {
+                case "M":
+                    //SharedMesh è il sottocomponente che mi permette di cambiare il MeshFilters.
+                    rightMeshLetter.sharedMesh = MeshesLetter[0].sharedMesh;
+                    break;
+                case "A":
+                    rightMeshLetter.sharedMesh = MeshesLetter[1].sharedMesh;
+                    break;
+                case "V":
+                    rightMeshLetter.sharedMesh = MeshesLetter[2].sharedMesh;
+                    break;
+                case "X":
+                    rightMeshLetter.sharedMesh = MeshesLetter[3].sharedMesh;
+                    break;
+                case "E":
+                    rightMeshLetter.sharedMesh = MeshesLetter[4].sharedMesh;
+                    break;
+                case "S":
+                    rightMeshLetter.sharedMesh = MeshesLetter[5].sharedMesh;
+                    break;
+                case "D":
+                    rightMeshLetter.sharedMesh = MeshesLetter[6].sharedMesh;
+                    break;
+                case "Z":
+                    rightMeshLetter.sharedMesh = MeshesLetter[7].sharedMesh;
+                    break;
+                case "W":
+                    rightMeshLetter.sharedMesh = MeshesLetter[8].sharedMesh;
+                    break;
+                case "C":
+                    rightMeshLetter.sharedMesh = MeshesLetter[9].sharedMesh;
+                    break;
+                default:
+                    Debug.Log("Non conosco la lettera");
+                    break;
+            }
+        }
+
+        //void ontriggerenter(Collider other){
+        //    if (other.tag == "PlayerBorder")
+        //    {
+        //        transform.LookAt(target);
+        //    }
+        //}
+    }
+}
 	
-	MeshFilter rightMeshLetter;
-	rightMeshLetter = GetComponentInChildren<MeshFilter>();
-	switch (rightLetter){ 
-			case "M":
-				//SharedMesh è il sottocomponente che mi permette di cambiare il MeshFilters.
-				rightMeshLetter.sharedMesh = MeshesLetter[0].sharedMesh; 
-				break;
-			case "A":
-				rightMeshLetter.sharedMesh = MeshesLetter[1].sharedMesh; 
-				break;
-			case "V":
-				rightMeshLetter.sharedMesh = MeshesLetter[2].sharedMesh; 
-				break;
-			case "X":
-				rightMeshLetter.sharedMesh = MeshesLetter[3].sharedMesh; 
-				break;
-			case "E":
-				rightMeshLetter.sharedMesh = MeshesLetter[4].sharedMesh; 
-				break;
-			case "S":
-				rightMeshLetter.sharedMesh = MeshesLetter[5].sharedMesh; 
-				break;
-			case "D":
-				rightMeshLetter.sharedMesh = MeshesLetter[6].sharedMesh; 
-				break;
-			case "Z":
-				rightMeshLetter.sharedMesh = MeshesLetter[7].sharedMesh; 
-				break;
-			case "W":
-				rightMeshLetter.sharedMesh = MeshesLetter[8].sharedMesh; 
-				break;
-			case "C":
-				rightMeshLetter.sharedMesh = MeshesLetter[9].sharedMesh; 
-				break;
-			default:
-				Debug.Log("Non conosco la lettera");
-				break;
-			}
-		}
-	}
-	}
