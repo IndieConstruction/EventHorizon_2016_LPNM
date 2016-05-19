2bc4e3e3a0270a050a1a7104ad9e6404d6008800
Marco Conio <marco.conio@gmail.com>
2016-05-19 11:18:22 +0200

    Prove bounce. fallita

--- UnityProject/Assets/Scripts/PlayerBounce.cs
+++ UnityProject/Assets/Scripts/PlayerBounce.cs
@@ -4,10 +4,15 @@ namespace EH.LPNM
 {
     public class PlayerBounce : MonoBehaviour
     {
+        public GameObject debugGO;
+
         Player p;
         Vector3 target;
         bool toCenter = false;
-        public float speed = 0.1f;
+        public float speed = 5.0f;
+        private float startTime;
+        private float journeyLength;
+        
         void Start()
         {
             p = FindObjectOfType<Player>();
@@ -18,19 +23,36 @@ namespace EH.LPNM
                 toCenter = false;
 
             if (toCenter == true)
-                p.transform.Translate(-target.x,0,0);
-            
-        }
+            {
+                float distCovered = (Time.time - startTime) * speed;
+                float fracJourney = distCovered / journeyLength;
 
+                p.transform.position = Vector3.Lerp(startPoint, (startPoint + endPoint), fracJourney);
+              //  p.transform.position = Vector3.Lerp(p.transform.position,target, 1);
+            }
+        }
+        Vector3 startPoint;
+        Vector3 endPoint;
         void OnTriggerExit(Collider other)
         {
             if (p != null)
             {
-               // float step = speed * Time.deltaTime;
-         
+               Vector3 startPoint = p.transform.position;
+                Vector3 endPoint = new Vector3(this.transform.position.x, startPoint.y, this.transform.position.z);
+                // float step = speed * Time.deltaTime;
+                startTime =Time.time;
+                journeyLength=Vector3.Distance(p.transform.position,this.transform.position);
+            
                 Debug.Log("uscito dal limite");
-                 target = Vector3.MoveTowards(p.transform.position, this.transform.position, speed).normalized;
+                
+              // target = ((this.transform.position.normalized-p.transform.position.normalized));
+                //target = Vector3.Reflect(p.transform.position, this.transform.position).normalized;
                 Debug.Log("pos " + target);
+               // GameController.Spawn(debugGO, target);
+               GameController.Spawn(debugGO, startPoint);
+               GameController.Spawn(debugGO, (endPoint - new Vector3(startPoint.x, 0, startPoint.z)));
+
+
                 //target=Vector3.n
                 toCenter = true;
             }
