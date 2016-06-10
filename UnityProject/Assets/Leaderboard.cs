e01bf1834895cade1ce0754726fdc8e1a51cb7a0
Marco Conio <marco.conio@gmail.com>
2016-06-10 12:04:16 +0200

    continui lavoro leaderboard

---
+++ UnityProject/Assets/Leaderboard.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Leaderboard : MonoBehaviour {
+	public string[] LeaderboardPoint= new string[10];
+	private string Temp;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void InitializeArray(){
+		foreach (string Point in LeaderboardPoint) {
+			Point = PlayerPrefs.GetString ();	
+		}
+	}
+
+	void GetRegister(){
+		Temp = PlayerPrefs.GetString ("Temp");
+		string[] TempSplit = Temp.Split ("|");
+		string TempName = TempSplit [0];
+		string TempScore = TempSplit [1];
+		int count = 0;
+		foreach (string Point in LeaderboardPoint) {
+			if (TempScore > Point) {
+				
+			}
+
+			count++;
+		}
+	}
+		void OrderArray()
+		{
+						
+		}
+
+
+	}
+
