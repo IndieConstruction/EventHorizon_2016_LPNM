387df72d550ec2719586ad83cb6c1defb14ba372
Brago <bragolab@gmail.com>
2016-06-15 10:11:45 +0200

    Speed aggiunta alla Ventola

---
+++ UnityProject/Assets/Propeller.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+namespace EH.LPNM{
+
+public class Propeller : Malus  {
+
+		public float speed = 1;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+			SpeedRotation();
+	
+	}
+
+		void SpeedRotation(){
+			Animator anm = GetComponent<Animator>();
+			anm.speed = speed; 
+
+		}
+}
+}
