be4469dce3ad25096608965bd6e36da6a3c9bf44
Marco Conio <marco.conio@gmail.com>
2016-06-21 15:07:05 +0200

    Update leaderboard, aggiunta immagine per sfondo menù

--- UnityProject/Assets/Scripts/Leaderboard.cs
+++ UnityProject/Assets/Scripts/Leaderboard.cs
@@ -3,7 +3,7 @@ using System.Collections;
 
 public class Leaderboard : MonoBehaviour {
 	public string[] LeaderboardPoint= new string[10];
-	private string Temp;
+	private string Save;
 	// Use this for initialization
 	void Start () {
 	
@@ -13,34 +13,50 @@ public class Leaderboard : MonoBehaviour {
 	void Update () {
 	
 	}
-	/*
-	void InitializeArray(){
+/*	void InitializeArray(){
 		foreach (string Point in LeaderboardPoint) {
 			Point = PlayerPrefs.GetString ();	
 		}
-	}
-	/*
+	}*/
+
 	void GetRegister(){
-		Temp = PlayerPrefs.GetString ("Temp");
+	/*	Temp = PlayerPrefs.GetString ("Temp");
 		string[] TempSplit = Temp.Split ("|");
 		string TempName = TempSplit [0];
 		string TempScore = TempSplit [1];
-		int count = 0;
-		foreach (string Point in LeaderboardPoint) {
-			if (TempScore > Point) {
-				
-			}
+*/
+		string TempName="",TempScore="";
+		SplitTemp("Temp",'|',TempName,TempScore);
 
-			count++;
+		foreach (string Point in LeaderboardPoint) {
+			string TempName2="", TempScore2="";
+			SplitTemp (Point, '|', TempName2, TempScore2);
+
+			if (int.Parse(TempScore) > int.Parse(TempScore2)) {
+				Save = Point;
+				break;
+			}
 		}
+
+
+
+
+
 	}
+
 		void OrderArray()
 		{
 						
 		}
 
+	void SplitTemp(string str, char del, string temp1, string temp2)
+	{
+		string temp = PlayerPrefs.GetString (str);
+		string[] TempSplit = temp.Split (del);
+		temp1 = TempSplit [0];
+		temp2 = TempSplit [1];
+	}
+
 
 	}
 
-*/
-}
