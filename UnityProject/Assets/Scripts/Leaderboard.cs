1fa78ca986f330a7442ea69897d808e1ef831a89
Marco Conio <marco.conio@gmail.com>
2016-07-06 17:32:44 +0200

    Continuo leaderboard

--- UnityProject/Assets/Scripts/Leaderboard.cs
+++ UnityProject/Assets/Scripts/Leaderboard.cs
@@ -32,40 +32,47 @@ public void GetRegister(){
 		TempScore=	ReturnScore ("Temp", '|');
 	//	SplitTemp("Temp",'|',TempName,TempScore);
     //    int n = 0;
-        if (LeaderboardPoint.Count>0)
+		if ((PlayerPrefs.GetString(LeaderboardPoint[9].ToString())).Length>1)
         {
-			int n = 0;
-            foreach (string Point in LeaderboardPoint)
+			//int n = 0;
+			int s = 0;
+			int p = 0;
+			int i = LeaderboardPoint.Count - 1;
+			for (;i >= 0 ; i--)
             {
-				string TempPointScore = PlayerPrefs.GetString(LeaderboardPoint.IndexOf(Point)+"");
-                //	SplitTemp (Point, '|', TempPointName, TempPointScore);
-				TempPointScore=	ReturnScore (n+"", '|');
-				n++;
-                if (TempScore.CompareTo(TempPointScore)>0)
-                {
-                    // PlayerPrefs.SetString(NameArrayLeaderboard + n, TempScore+"|"+TempName);
-                    Save = Point;
-                    break;
-                }
+				if ((PlayerPrefs.GetString(LeaderboardPoint[i].ToString())).Length>1)
+				{
+					//string TempPointScore = PlayerPrefs.GetString (LeaderboardPoint.IndexOf (Point) + "");
+					//	SplitTemp (Point, '|', TempPointName, TempPointScore);
+					string TempPointScore =	ReturnScore (i + "", '|');
+					//n++;
+					p = int.Parse (TempPointScore);
+					s = int.Parse (TempScore);
+					if (s>p) {
+						// PlayerPrefs.SetString(NameArrayLeaderboard + n, TempScore+"|"+TempName);
+						Save = LeaderboardPoint[i].ToString();
+						break;
+					}
+				}
             }
 			if (Save != null) {
-				
-				OrderArray (LeaderboardPoint, Save, TempName);
+				RenamePrefs (LeaderboardPoint, Save);
+				//OrderArray (LeaderboardPoint, Save, TempName);
 				PlayerPrefs.SetString (LeaderboardPoint.IndexOf(Save)+"",TempName+'|'+ TempScore);
 				PlayerPrefs.Save ();
 
-			} else if (LeaderboardPoint.Count < 10) {
-				int count = LeaderboardPoint.Count+1;
-				LeaderboardPoint.Add(count);
-				PlayerPrefs.SetString(count+"", TempName+'|'+ TempScore);
+			} else {
+				//int count = LeaderboardPoint.Count+1;
+				//LeaderboardPoint.Add(count);
+					PlayerPrefs.SetString(i+1+"", TempName+'|'+ TempScore);
 				PlayerPrefs.Save();
 			}
 		}
         else
         {
-			int count = LeaderboardPoint.Count;
-			LeaderboardPoint.Add(count);
-			PlayerPrefs.SetString(count+"", TempName+'|'+ TempScore);
+			//int count = LeaderboardPoint.Count;
+			//LeaderboardPoint.Add(count);
+			PlayerPrefs.SetString(9+"", TempName+'|'+ TempScore);
             PlayerPrefs.Save();
         }
 
@@ -74,7 +81,16 @@ public void GetRegister(){
 
 	}
 
-	public void RenamePrefs(){
+	public void RenamePrefs(ArrayList Array,string Obj){
+		int ind = Array.IndexOf (Obj);
+		foreach (string Ar in Array)
+			Debug.Log (PlayerPrefs.GetString(Ar));
+		for (int i = 0; i > ind; i++) {
+			PlayerPrefs.SetString (Array [i].ToString(), Array [i + 1].ToString());	
+		}
+		foreach (string Ar in Array)
+			Debug.Log (PlayerPrefs.GetString(Ar));
+
 	}
 
  public void StampArray()
@@ -84,6 +100,7 @@ public void GetRegister(){
         //   foreach (string Point in LeaderboardPoint)
        for(int i=0;i<LeaderboardPoint.Count-1;i++)
         {
+			if ((PlayerPrefs.GetString(LeaderboardPoint[i].ToString())).Length>1)
 			Debug.Log("Name: "+ReturnName(i+"",'|')+" Point: "+ReturnScore(i+"",'|'));
         }
 
@@ -98,9 +115,9 @@ public void GetRegister(){
 	void SplitTemp(string str, char del, string temp1, string temp2)
 	{
 		string temp = PlayerPrefs.GetString (str);
-		string[] TempSplit = temp.Split (del);
-		temp1 = TempSplit [0];
-		temp2 = TempSplit [1];
+		string[] TempSplit3 = temp.Split (del);
+		temp1 = TempSplit3 [0];
+		temp2 = TempSplit3 [1];
 	}
 
 	string ReturnName(string str, char del)
@@ -113,8 +130,8 @@ public void GetRegister(){
 	string ReturnScore(string str, char del)
 	{
 		string temp = PlayerPrefs.GetString (str);
-		string[] TempSplit = temp.Split (del);
-		return TempSplit [1];
+		string[] TempSplit2 = temp.Split (del);
+		return TempSplit2 [1];
 	}
 
 	}
